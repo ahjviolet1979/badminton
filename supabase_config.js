bac4fba541534bf22fdca946bc591de6aa8526f9
ahjviolet1979 <ahjviolet@naver.com>
2026-05-22 18:05:27 +0900

    Add files via upload

--- supabase_config.js
+++ supabase_config.js
@@ -1,8 +1,8 @@
 window.BADMINTON_SUPABASE = {
   url: "https://ilvdjnzqgcpxguogbert.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsdmRqbnpxZ2NweGd1b2diZXJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MzU3OTQsImV4cCI6MjA5NTAxMTc5NH0._X2gQ3pk1hvzTV1xwT5PtAHRDfcKlXoqBizvzvnj8YQ",
-  eventId: "ybw260606",
+  eventId: "main",
   adminId: "admin",
   adminPassword: "1234",
-  viewerPassword: "ybw260606"
+  viewerPassword: "260606"
 };
